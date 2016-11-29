21afc65c09e5ecaf7f8183db69ff8a47d074174e
JT Burke <jtburke88@googlemail.com>
2016-11-29 17:59:44 +0000

    Removes temp ConsoleApp. Displays nicer welcome and done messages.

--- adapter-app/src/main/java/com/github/jtburke/sainsburystest/adapter/app/ConsoleApp.java
+++
@@ -1,7 +0,0 @@
-package com.github.jtburke.sainsburystest.adapter.app;
-
-public class ConsoleApp {
-    public ConsoleApp() {
-        System.out.println("Application Started");
-    }
-}
--- adapter-app/src/main/java/com/github/jtburke/sainsburystest/adapter/app/ConsoleAppRunner.java
+++ adapter-app/src/main/java/com/github/jtburke/sainsburystest/adapter/app/ConsoleAppRunner.java
@@ -3,8 +3,23 @@ package com.github.jtburke.sainsburystest.adapter.app;
 import java.util.Scanner;
 
 public class ConsoleAppRunner {
+    private static final String HORIZONTAL_RULE = "============================================================";
+
     public static void main(String[] args) throws Exception {
-        new ConsoleApp();
+        displayWelcomeMessage();
+        displayCloseMessageAndWaitForUserInput();
+    }
+
+    private static void displayWelcomeMessage() {
+        System.out.println("John-Terrance Burke");
+        System.out.println(HORIZONTAL_RULE);
+        System.out.println("Application started...");
+        System.out.println(HORIZONTAL_RULE + "\n");
+    }
+
+    private static void displayCloseMessageAndWaitForUserInput() {
+        System.out.println("\n" + HORIZONTAL_RULE);
+        System.out.println("Press [Enter] to close.");
         new Scanner(System.in).nextLine();
     }
 }
